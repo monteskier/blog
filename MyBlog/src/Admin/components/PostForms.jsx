5cfd9809d088e20feaf553a104a5a4d072b2523f
Joaquim Montoliu Aguilera <montoliuaj@administracio.local>
2024-03-09 07:41:49 +0100

    Echos los views y laytout de admin

--- MyBlog/src/Admin/components/PostForms.jsx
+++ MyBlog/src/Admin/components/PostForms.jsx
@@ -1,20 +1,30 @@
 export const PostForms = () => {
   return (
     <form action="">
-      <div className="form-floating mb-3">
-        <input type="text" className="form-control" id="title" name="title" placeholder="Titulo de la entrada" />
-        <label  htmlFor="title">Titulo</label>
+      <div className="row">
+        <div className="col-12">				
+          <div className="d-flex gap-2 justify-content-end mb-3">
+            <button className="btn btn-primary" type="button">Guardar</button>
+            <button className="btn btn-danger" type="button">Eliminar</button>
+          </div>
+        </div>
       </div>
-      <div className="form-floating mb-3">
-        <textarea className="form-control" name="description" id="description" placeholder="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem, ab!"></textarea>
-        <label  htmlFor="description">Entrada</label>
-      </div>
-      <div className="d-flex justify-content-end">
-        <button>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
-            <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
-          </svg>   	Enviar
-        </button>
+      <div className="row">
+        <div className="form-floating mb-3">
+          <input type="text" className="form-control" id="title" name="title" placeholder="Titulo de la entrada" />
+          <label  htmlFor="title">Titulo</label>
+        </div>
+        <div className="form-floating mb-3">
+          <textarea className="form-control" name="description" id="description" placeholder="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem, ab!"></textarea>
+          <label  htmlFor="description">Entrada</label>
+        </div>
+        <div className="d-flex justify-content-end">
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
+              <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+            </svg>   	Enviar
+          </button>
+        </div>
       </div>
     </form>
   )
